bac6e679d62a3dac997fcfd3a194099fef13700b
Tim Dukhnovskiy <dukhnovs@uw.edu>
2019-03-17 23:33:48 -0700

    added unit_test with sample API call

--- src/unit_test.cc
+++ src/unit_test.cc
@@ -20,4 +20,19 @@ namespace {
     // although testing user interfaces is notoriously 
     // difficult.
 
+    // test problem 1
+    TEST(API, GetWeatherDataSample) {
+        Client c;
+        json data;
+        c.get("api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=d0fcd7e3aea1a3bc4f2c6095671223e8", data, [this](json& response) {
+            cout<<response.dump()<<endl;
+        });
+        // wait for response
+        while(target_responses != 1) {
+        }
+        // std::this_thread::sleep_for(std::chrono::seconds(1));
+        EXPECT_EQ(1,c.num_responses());
+        c.process_responses();
+    }
+
 }
